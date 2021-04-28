3458db7e8a3c9e7943d6fc0ecd66eac296a12701
serjkovalko <serjkovalko@gmail.com>
2021-04-28 16:09:06 +0300

    start project

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+	
+// 	// Подключение анимации при скроле страници
+// $(function () {
+//   wow = new WOW(
+//     {
+//       boxClass: 'wow',      // default
+//       animateClass: 'animate__animated', // default
+//       offset: 0,          // default
+//       mobile: true,       // default
+//       live: true        // default
+//     }
+//   )
+//   wow.init();
+// });
+
+})
